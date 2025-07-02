c4531b31d02f8032bd2568b98bdb53342f747dd0
KeinerTorres <keiner.torres1@udea.edu.co>
2025-07-02 15:33:12 -0500

    Create luchador.cpp

---
+++ luchador.cpp
@@ -0,0 +1,52 @@
+#include "luchador.h"
+#include <QtMath>
+
+Luchador::Luchador(float x, float y, float ancho, float alto)
+    : Entidad(x, y, ancho, alto),
+    vida(100.0f), dañoBase(10.0f),
+    estaAtacando(false), cooldownAtaque(0.0f),
+    velocidadSalto(-500.0f), enSuelo(true)
+{}
+
+void Luchador::recibirDaño(float daño) {
+    vida -= daño;
+    if (vida < 0) vida = 0;
+}
+
+bool Luchador::estaVivo() const {
+    return vida > 0;
+}
+
+void Luchador::saltar() {
+    if (estaEnElSuelo()) {
+        velocidadY = velocidadSalto;
+        enSuelo = false;
+    }
+}
+
+void Luchador::actualizar(float deltaTiempo) {
+    aplicarGravedad(deltaTiempo);
+    posX += velocidadX * deltaTiempo;
+    posY += velocidadY * deltaTiempo;
+
+    if (posY >= 400) { // piso imaginario
+        posY = 400;
+        velocidadY = 0;
+        enSuelo = true;
+    }
+
+    if (cooldownAtaque > 0) {
+        cooldownAtaque -= deltaTiempo;
+        if (cooldownAtaque < 0) cooldownAtaque = 0;
+    }
+}
+
+void Luchador::aplicarGravedad(float deltaTiempo) {
+    if (!estaEnElSuelo()) {
+        velocidadY += 980 * deltaTiempo; // gravedad aproximada
+    }
+}
+
+bool Luchador::estaEnElSuelo() const {
+    return enSuelo;
+}
