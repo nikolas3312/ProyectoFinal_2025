980abe7ba5366e3741e1bd1246f04c167b7410c7
KeinerTorres <keiner.torres1@udea.edu.co>
2025-07-02 15:33:07 -0500

    Create luchador.h

---
+++ luchador.h
@@ -0,0 +1,31 @@
+#ifndef LUCHADOR_H
+#define LUCHADOR_H
+
+#include "entidad.h"
+
+class Luchador : public Entidad
+{
+protected:
+    float vida;
+    float dañoBase;
+    bool estaAtacando;
+    float cooldownAtaque;
+    float velocidadSalto;
+    bool enSuelo;
+
+public:
+    Luchador(float x, float y, float ancho, float alto);
+    virtual ~Luchador() {}
+
+    virtual void recibirDaño(float daño);
+    virtual bool estaVivo() const;
+    virtual void saltar();
+    virtual void actualizar(float deltaTiempo) override;
+    virtual void atacar() = 0; // Pure virtual, se define en hijos
+
+protected:
+    void aplicarGravedad(float deltaTiempo);
+    bool estaEnElSuelo() const;
+};
+
+#endif // LUCHADOR_H
