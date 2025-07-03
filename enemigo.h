ea5ebeda92ba1dbbfb83c62037c64489a251b71e
KeinerTorres <keiner.torres1@udea.edu.co>
2025-07-03 00:41:12 -0500

    Create enemigo.h

---
+++ enemigo.h
@@ -0,0 +1,24 @@
+#ifndef ENEMIGO_H
+#define ENEMIGO_H
+
+#include "luchador.h"
+
+enum class EstadoIA { Esperar, Atacar, Defender };
+
+class Enemigo : public Luchador
+{
+private:
+    EstadoIA estadoIA;
+    float temporizadorEstado;
+
+public:
+    Enemigo(float x, float y, float ancho, float alto);
+
+    void actualizar(float deltaTiempo) override;
+    void atacar() override;
+
+private:
+    void decidirAccion();
+};
+
+#endif // ENEMIGO_H
