91f59084de004adde83ce489de03628ff54c6b71
KeinerTorres <keiner.torres1@udea.edu.co>
2025-07-06 02:21:58 -0500

    Update nivel2_3.h

--- nivel2_3.h
+++ nivel2_3.h
@@ -2,32 +2,53 @@
 #define NIVEL2_3_H
 
 #include "personajejugador.h"
+#include "Nivel.h"
 #include "enemigo.h"
 #include "hitbox.h"
 #include <vector>
 #include <QPainter>
+#include <Qset>
 
-enum class EstadoJuego { Jugando, Ganado, Perdido };
 
-class Nivel2_3
-{
+class Nivel2_3 : public Nivel {
 private:
+    enum class Estado {
+        CUENTA_REGRESIVA,
+        JUGANDO,
+        VICTORIA,
+        DERROTA
+    };
+
+    Estado estadoActual;
+    PersonajeSeleccionado personajeElegido;
+
     PersonajeJugador* jugador;
     Enemigo* enemigo;
     std::vector<Hitbox*> hitboxesActivas;
-    EstadoJuego estado;
+    float tiempoCuentaRegresiva;
+    int numeroNivel; // esta es para saber si es contra ten shin han o piccolo
 
-public:
-    Nivel2_3();
-    ~Nivel2_3();
-
-    void actualizar(float deltaTiempo, const QSet<int>& teclas);
-    void dibujar(QPainter* painter);
-    EstadoJuego getEstadoJuego() const;
-
-private:
+    void actualizarCuentaRegresiva(float deltaTiempo);
+    void actualizarCombate(float deltaTiempo, const Qset<int>& teclas);
     void revisarColisiones();
     void limpiarHitboxes();
+
+
+public:
+    Nivel2_3(personajeSeleccionado personaje, int numeroNivel);
+    ~Nivel2_3();
+
+    void iniciaizar() override;
+    void actualizar(float deltaTiempo) override;
+    void dibujar(QPainter* painter) override;
+    bool estaTerminado() const override;
+
+
+    void recibirInput(const Qset<int>& teclas);
+
+private:
+    Qset<int> teclasPresionadas;
+
 };
 
 #endif // NIVEL2_3_H
